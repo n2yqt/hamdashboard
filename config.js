18f07741e03e5845e3701f8e46c829887eb004ca
n2yqt <ryan.tourge@gmail.com>
2024-07-20 09:20:01 -0400

    Updated APRS MAP URL
    Updated APRS MAP URL

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ const aIMG = [
   ],
   [
     "APRS",
-    "https://dashboard.tourge.net/map.png",
+    "https://n2yqt.tourge.net/dashboard/hamdashboard/map.png",
   ],
   [
     "AURORA/NOAA D-RAP",
